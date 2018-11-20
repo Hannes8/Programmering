3f8d623476c75232ec2042f408a1567ddbe432ba
Hannes8 <hannes.kaulio@gmail.com>
2018-11-20 09:41:24 +0100

    Update uppgift1.java

--- uppgift1.java
+++ uppgift1.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+//	Skapa ett program som läser in tal från konsolen och när man skriver in talet 0 ska den skriva ut summan av alla tal.
 
 public class uppgift1 {
+	public static void main(String[] args) {
 
+	
+	
+	
+	double tal=1;
+	double summa=0;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Please type a nummber");
+	
+	
+	while (tal != 0) {
+		tal=input.nextDouble();
+		summa +=tal;	
+	
+	
+	}
+	System.out.println("The sum of all nummber is " + summa);
+	input.close();
+	}
 }
